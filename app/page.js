543a7384015a7ddd324f6e44a95dc18d60375dfd
Your Name <joesvetz@gmail.com>
2024-12-12 10:23:27 -0700

    Add LoomRecorder to homepage

--- app/page.js
+++ app/page.js
@@ -1,16 +1,11 @@
 "use client";
 
-import dynamic from 'next/dynamic';
-
-const LoomRecorder = dynamic(
-  () => import('./components/LoomRecorder'),
-  { ssr: false } // This is important!
-);
+import LoomRecorder from './components/LoomRecorder'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <LoomRecorder />
     </main>
-  );
+  )
 }
